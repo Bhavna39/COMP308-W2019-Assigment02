3c779c01fe63578e362c841a34a3a8c2da8d1fe6
Bhavna Pulliahgari <43325994+BhavnaP@users.noreply.github.com>
2019-03-28 01:58:12 -0400

    added contact-list using mongoose

--- routes/contact.js
+++ routes/contact.js
@@ -15,13 +15,12 @@ router.get('/', (req, res, next) =>{
           return console.error(err);
       }else{
           console.log(contactList);
-
-          /*
+          
           res.render('contacts/index', {
               title: 'Contact List',
               contactList: contactList
           });
-          */
+        
       }
     });
 });
