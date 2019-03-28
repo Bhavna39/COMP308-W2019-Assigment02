626e3530287f97f1dbb528e271aa358fbdb2c4d1
Bhavna Pulliahgari <43325994+BhavnaP@users.noreply.github.com>
2019-03-28 12:21:51 -0400

    added JWT configuration

--- server/config/app.js
+++ server/config/app.js
@@ -10,6 +10,19 @@ let express = require("express");
 let path = require("path");
 let cookieParser = require("cookie-parser");
 let logger = require("morgan");
+let cors = require('cors');
+
+// modules for authentication
+let session = require('express-session');
+let passport = require('passport');
+
+let passportJWT = require('passport-jwt');
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
+
+let passportLocal = require('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
 
 // database setup
 let mongoose = require('mongoose');
@@ -41,8 +54,53 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../../public")));
 app.use(express.static(path.join(__dirname, "../../node_modules")));
 
-app.use("/", indexRouter);
-app.use("/contact-list", contactRouter);
+app.use(cors());
+
+// setup express-session
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: false,
+  resave: false
+}));
+
+// initialize flash
+app.use(flash());
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// create a User model
+let userModel = require('../models/user');
+let User = userModel.User;
+
+// implement a User authetication strategy
+passport.use(User.createStrategy());
+
+// serialize and deserialize the User info
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = DB.secret;
+
+let strategy = new JWTStrategy(jwtOptions, (jwt_payload, done)=>{
+  User.findById(jwt_payload.id)
+  .then(user => {
+    return done(null, user);
+  })
+  .catch(err => {
+    return done(err, false);
+  });
+});
+
+passport.use(strategy);
+
+app.use("/api", indexRouter);
+app.use("/api/contact-list", contactRouter); // protect this section
+
+//capture - random links/ inccorect url - redirect to home page
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
