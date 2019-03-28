f639c1c125df274460a11ca5a06ffcb29998af78
Bhavna Pulliahgari <43325994+BhavnaP@users.noreply.github.com>
2019-03-28 01:21:15 -0400

    added db

---
+++ config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "URI" : "mongodb://localhost/test",
+    //"URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson4b",
+     //"URI" : "mongodb://bhavna:123456abc@ds042138.mlab.com:42138/comp308-w2019-assignment02"
+     "secret": "SomeSecret"
+ }
